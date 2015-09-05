77250c447095140168723916547db78532d39c5b
ryanhoo <ryan.hoo.j@gmail.com>
2015-09-05 23:44:48 +0800

    feat(Sample): Update sample code

--- app/src/main/java/org/hybridsquad/android/photocropper/TestActivity.java
+++ app/src/main/java/org/hybridsquad/android/photocropper/TestActivity.java
@@ -3,11 +3,13 @@ package org.hybridsquad.android.photocropper;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
-import org.hybridsquad.android.library.BasePhotoCropActivity;
+import org.hybridsquad.android.library.BitmapUtil;
+import org.hybridsquad.android.library.CropHandler;
 import org.hybridsquad.android.library.CropHelper;
 import org.hybridsquad.android.library.CropParams;
 
@@ -18,19 +20,19 @@ import org.hybridsquad.android.library.CropParams;
  * Time: 11:44 AM
  * Desc: TestActivity
  */
-public class TestActivity extends BasePhotoCropActivity implements View.OnClickListener {
+public class TestActivity extends AppCompatActivity implements CropHandler, View.OnClickListener {
 
     public static final String TAG = "TestActivity";
 
     ImageView mImageView;
 
-    CropParams mCropParams = new CropParams();
+    CropParams mCropParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test);
-
+        mCropParams = new CropParams(this);
         mImageView = (ImageView) findViewById(R.id.image);
 
         findViewById(R.id.bt_crop_capture).setOnClickListener(this);
@@ -41,34 +43,51 @@ public class TestActivity extends BasePhotoCropActivity implements View.OnClickL
 
     @Override
     public void onClick(View v) {
+        mCropParams.refreshUri();
         switch (v.getId()) {
             case R.id.bt_crop_capture: {
                 mCropParams.enable = true;
-                Intent intent = CropHelper.buildCaptureIntent(mCropParams.uri);
+                mCropParams.compress = false;
+                Intent intent = CropHelper.buildCameraIntent(mCropParams);
                 startActivityForResult(intent, CropHelper.REQUEST_CAMERA);
             }
             break;
             case R.id.bt_crop_gallery: {
                 mCropParams.enable = true;
-                // MUST!! Clear Last Cached Image
-                CropHelper.clearCachedCropFile(mCropParams.uri);
-                startActivityForResult(CropHelper.buildCropFromGalleryIntent(mCropParams), CropHelper.REQUEST_CROP);
+                mCropParams.compress = false;
+                Intent intent = CropHelper.buildGalleryIntent(mCropParams);
+                startActivityForResult(intent, CropHelper.REQUEST_CROP);
             }
             break;
             case R.id.bt_capture: {
                 mCropParams.enable = false;
-                Intent intent = CropHelper.buildCaptureIntent(mCropParams.uri);
+                mCropParams.compress = true;
+                Intent intent = CropHelper.buildCameraIntent(mCropParams);
                 startActivityForResult(intent, CropHelper.REQUEST_CAMERA);
             }
             break;
             case R.id.bt_gallery: {
                 mCropParams.enable = false;
-                CropHelper.clearCachedCropFile(mCropParams.uri);
-                Intent intent = CropHelper.buildPickIntent(mCropParams.uri);
+                mCropParams.compress = true;
+                Intent intent = CropHelper.buildGalleryIntent(mCropParams);
                 startActivityForResult(intent, CropHelper.REQUEST_PICK);
             }
             break;
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        CropHelper.handleResult(this, requestCode, resultCode, data);
+        if (requestCode == 1) {
+            Log.e(TAG, "");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        CropHelper.clearCacheDir();
+        super.onDestroy();
     }
 
     @Override
@@ -78,18 +97,30 @@ public class TestActivity extends BasePhotoCropActivity implements View.OnClickL
 
     @Override
     public void onPhotoCropped(Uri uri) {
+        // Original or Cropped uri
         Log.d(TAG, "Crop Uri in path: " + uri.getPath());
-        // Toast.makeText(this, "Photo cropped!", Toast.LENGTH_LONG).show();
-        mImageView.setImageBitmap(CropHelper.decodeUriAsBitmap(this, mCropParams.uri));
+        if (!mCropParams.compress)
+            mImageView.setImageBitmap(BitmapUtil.decodeUriAsBitmap(this, uri));
     }
 
     @Override
-    public void onCropCancel() {
+    public void onCompressed(Uri uri) {
+        // Compressed uri
+        mImageView.setImageBitmap(BitmapUtil.decodeUriAsBitmap(this, uri));
+    }
+
+    @Override
+    public void onCancel() {
         Toast.makeText(this, "Crop canceled!", Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public void onCropFailed(String message) {
-        Toast.makeText(this, "Crop failed:" + message, Toast.LENGTH_LONG).show();
+    public void onFailed(String message) {
+        Toast.makeText(this, "Crop failed: " + message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void handleIntent(Intent intent, int requestCode) {
+        startActivityForResult(intent, requestCode);
     }
 }
